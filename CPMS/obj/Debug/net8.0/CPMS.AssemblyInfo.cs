1d8efde57359b593410c7583a84401d109de419c
kuanbase <lamkuan32@gmail.com>
2025-03-26 18:03:25 +0800

    update something

--- CPMS/obj/Debug/net8.0/CPMS.AssemblyInfo.cs
+++ CPMS/obj/Debug/net8.0/CPMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aeb243f9a48f8a46a4c795736333aae399ac265")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
